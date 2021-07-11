5510f6c6a8fe3ac704de14b300be10adffeec5c6
Aybatu <aybatu.kerkukluoglu@gmail.com>
2021-07-11 13:36:37 +0300

    Calculator Initial

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var calcPressed: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcPressed(_ sender: UIButton) {
+        if calcPressed {
+            displayLabel.text = sender.currentTitle
+            calcPressed = false
+        } else {
+            displayLabel.text = sender.currentTitle
+        }
+    }
+    
+    
+    @IBAction func numPressed(_ sender: UIButton) {
+        displayLabel.text = sender.currentTitle
+    }
+    
 }
 
